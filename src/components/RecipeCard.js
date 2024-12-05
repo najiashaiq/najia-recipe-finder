5ea0593b02ab15b6bb16b4f94ab994cefb928d1c
najiashaiq <najiashaiq786@gmail.com>
2024-12-05 10:15:42 -0500

    first commit

---
+++ src/components/RecipeCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/styles/RecipeCard.css';
+
+const RecipeCard = ({ recipe }) => {
+  return (
+    <div className="recipe-card">
+      <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+      <h3>{recipe.strMeal}</h3>
+      <Link to={`/recipe/${recipe.idMeal}`}>View Recipe</Link>
+    </div>
+  );
+};
+
+export default RecipeCard;
